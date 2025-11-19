68fb0495c9a957bc551778328df7bb6757c7f9ee
Abhay Shah <seraski2021@gmail.com>
2025-11-19 15:58:13 +0545

    removed the testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <HowItWorks />
         <WhatWillWeDo />
         <OurProduct />
-        <Testimonials />
+        {/* <Testimonials /> */}
       </main>
       <Footer />
     </>
